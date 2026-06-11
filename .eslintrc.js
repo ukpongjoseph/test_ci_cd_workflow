274ed7c5086edbe1bbb3e7745b35f925b35a8400
ukpongjoseph <jukpong499@gmail.com>
2026-06-11 02:41:57 +0100

    Adjusted few eslint errors

--- .eslintrc.js
+++ .eslintrc.js
@@ -33,7 +33,7 @@ module.exports = {
     ],
     "semi": [
       "error",
-      "never"
+      "off"
     ],
     "eqeqeq": "error",
     "no-trailing-spaces": "error",
@@ -43,7 +43,7 @@ module.exports = {
     "arrow-spacing": [
       "error", { "before": true, "after": true }
     ],
-    "no-console": "error",
+    "no-console": "off",
     "react/prop-types": 0
   }
 }
